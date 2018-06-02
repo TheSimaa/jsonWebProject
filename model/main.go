9018945fbd7ec13e9fdef879681ab0c6ecd14f27
maximprytyka <maxympritika@gmail.com>
2018-06-02 23:49:14 +0300

    fix POST and PUT

--- model/main.go
+++ model/main.go
@@ -13,7 +13,7 @@ import (
 var users []User
 
 type User struct {
-	ID        int `json:"id"`
+	ID        int    `json:"id"`
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 	Age       string `json:"age"`
@@ -54,19 +54,48 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		if item.ID == idUser {
 			json.NewEncoder(w).Encode(item)
 		}
-	}a
+	}
 
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("Content-Type", "application/json")
 	var user User
-	json.NewDecoder(r.Body).Decode(&user)
+
+	r.ParseForm()
 
 	user.ID = users[len(users)-1].ID + 1
+	user.Age = r.Form.Get("age")
+	user.Email = r.Form.Get("email")
+	user.Firstname = r.Form.Get("firstname")
+	user.Lastname = r.Form.Get("lastname")
+	user.Location = r.Form.Get("location")
 
 	users = append(users, user)
 
+	json.NewEncoder(w).Encode(users)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request)  {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	r.ParseForm()
+	idUser, _ := strconv.Atoi(params["id"])
+
+	for index,item := range users{
+
+		if item.ID == idUser{
+			users[index].Age = r.Form.Get("age")
+			users[index].Email = r.Form.Get("email")
+			users[index].Firstname = r.Form.Get("firstname")
+			users[index].Lastname = r.Form.Get("lastname")
+			users[index].Location = r.Form.Get("location")
+		}
+
+	}
 	json.NewEncoder(w).Encode(users)
 }
 
@@ -78,8 +107,8 @@ func main() {
 	r.HandleFunc("/api/users", getUsers).Methods("GET")
 	r.HandleFunc("/api/user/{id}", getUser).Methods("GET")
 	r.HandleFunc("/api/user", createUser).Methods("POST")
-	//r.HandleFunc("/api/user/{id}", updateUser).Methods("PUT")
-	//r.HandleFunc("/api/user/{id}", delпeteUser).Methods("DELETE")
+	r.HandleFunc("/api/user/{id}", updateUser).Methods("PUT")
+	//r.HandleFunc("/api/user/{id}", deleteUser).Methods("DELETE")
 
 	http.HandleFunc("/", nil)
 	http.ListenAndServe(":8000", r)
